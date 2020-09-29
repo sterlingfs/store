9d4907e8e95e074da9f28374e5b9c08891b8ac7e
Brady Sterling <sterlingfsio@gmail.com>
2020-09-29 01:43:39 +0000

    Store dir

---
+++ src/Store.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Store = void 0;
+var Store = /** @class */ (function () {
+    function Store() {
+        this.status = 'init';
+    }
+    return Store;
+}());
+exports.Store = Store;
